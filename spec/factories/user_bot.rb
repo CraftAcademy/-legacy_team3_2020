0b9f9e991c274498b9c290fef8792eef064db9fa
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-13 17:47:56 +0100

    writing the test to see that a user can log in and passing that

--- spec/factories/user_bot.rb
+++ spec/factories/user_bot.rb
@@ -4,10 +4,4 @@ FactoryBot.define do
         email { "john@mail.com" }
         password { "12345678" }
     end
-
-    factory :usertwo, class: User do
-        name { "Jane Doe" }
-        email { "jane@mail.com" }
-        password { "87654321" }
-    end
 end
--- spec/features/user_can_login_to_account_spec.rb
+++ spec/features/user_can_login_to_account_spec.rb
@@ -1,4 +1,16 @@
-featur 'User can' do
-    
+feature 'User can' do
+    let(:registered_user) { create(:user) }
+
+    before do
+        visit root_path
+    end
+
+    it 'log in' do
+        click_on "Login"
+        fill_in "user_email", with: registered_user.email
+        fill_in "user_password", with: "12345678"
+        click_on "Log in"
+        expect(page).to have_content "Hello"
+    end
     
 end
